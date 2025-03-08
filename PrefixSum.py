dd66fb0274b36cef26c21b726aef509ec1820e2d
alibahrami2 <alibahrami.devops@gmail.com>
2025-03-08 21:35:51 +0330

    first commit

---
+++ PrefixSum.py
@@ -0,0 +1,14 @@
+def PrefixSum(arr):
+    n = len(arr)
+    prefix = [0] * n
+    prefix[0] = arr[0]
+    for i in range(1 , n):
+        print("arr " ,arr[i])
+        print("prefix " ,prefix[i])
+        prefix[i] = prefix[i - 1] + arr[i]
+        print(prefix[i])
+    return prefix
+
+arr = [2, 5, 3, 0, 8]
+prefix = PrefixSum(arr)
+print(prefix)
